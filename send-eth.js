489e51bd7dc369de52a8f5b87c490c7709ded735
Wallace <wallacesip@gmail.com>
2023-09-14 17:01:28 -0300

    added approve USDT token for auto distribute transaction

--- send-eth.js
+++ send-eth.js
@@ -10,27 +10,35 @@ const wallet = new ethers.Wallet(privateKey, provider);
 
 // Define the contract address
 const contractAddress = '0xE03F0368bee4e95421A7968aa082E6d965F7C7C0'; // Replace with your contract address
+const USDT_CONTRACT_ADDRESS = '0xc2132D05D31c914a87C6611C10748AEb04B58e8F'; 
+const USDT_ABI = require('./Usdt.json');
+const usdtContract = new ethers.Contract(USDT_CONTRACT_ADDRESS, USDT_ABI, wallet);
 
-// Function to send 0 ETH to the contract
-async function sendZeroETH() {
+// Function to send 0 ETH to the contract and approve USDT transfer
+async function sendZeroETHAndApprove() {
   try {
-   // Fetch current gas prices from an Ethereum gas oracle
-   const gasPrices = await axios.get(`https://api.polygonscan.com/api?module=gastracker&action=gasoracle&apikey=${process.env.SCAN_API_KEY}`);
+    // Fetch current gas prices from an Ethereum gas oracle
+    const gasPrices = await axios.get(`https://api.polygonscan.com/api?module=gastracker&action=gasoracle&apikey=${process.env.SCAN_API_KEY}`);
 
-   // Use the recommended gas price for the transaction (in gwei)
-   const gasPrice = gasPrices.data.result.FastGasPrice; // Use "fast" gas price for optimal speed
+    // Use the recommended gas price for the transaction (in gwei)
+    const gasPrice = gasPrices.data.result.FastGasPrice; // Use "fast" gas price for optimal speed
 
-   const tx = await wallet.sendTransaction({
-     to: contractAddress,
-     value: 0,
-     gasLimit: 800000, // Set your custom gas limit here
-     gasPrice: ethers.utils.parseUnits(gasPrice, 'gwei') // Convert to BigNumber
-   });
+    // Approve USDT transfer with maxUint256
+    const approveTx = await usdtContract.approve(contractAddress, ethers.constants.MaxUint256);
+    await approveTx.wait();
 
-   await tx.wait();
-   console.log(`Auto Distribute Transaction sent, TX HASH: ${tx.hash}`);
-   console.log(`Gas Gwei used: ${gasPrice}`);
-   return true;
+    // Send 0 ETH to the contract
+    const tx = await wallet.sendTransaction({
+      to: contractAddress,
+      value: 0,
+      gasLimit: 800000, // Set your custom gas limit here
+      gasPrice: ethers.utils.parseUnits(gasPrice, 'gwei'), // Convert to BigNumber
+    });
+
+    await tx.wait();
+    console.log(`Auto Distribute Transaction sent, TX HASH: ${tx.hash}`);
+    console.log(`Gas Gwei used: ${gasPrice}`);
+    return true;
   } catch (error) {
     console.error('Error sending transaction:', error);
     return false;
@@ -39,5 +47,5 @@ async function sendZeroETH() {
 
 // Schedule the transaction to run every 24 hours
 cron.schedule('0 0 */1 * * *', () => {
-  sendZeroETH();
+  sendZeroETHAndApprove();
 });
